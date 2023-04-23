5249102ab67c2f8016aaf9c1c4cc51b46d591ceb
Nader El-Ghotmi <43392069+NaderEG@users.noreply.github.com>
2023-04-23 06:47:16 -0400

    linear fib

--- 02_Fibonacci.py
+++ 02_Fibonacci.py
@@ -8,4 +8,17 @@ def fib1(n):
 	else:
 		return fib1(n - 1) + fib1(n - 2)
 
-print(fib1(int(input())))
+def fib2(n):
+	if n == 0:
+		return 0
+
+	f = []
+
+	f.append(0)
+	f.append(1)
+
+	for i in range(2, n+1):
+		f.append(f[i-1] + f[i-2])
+	return f[n]
+
+print(fib2(int(input())))
